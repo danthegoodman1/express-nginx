f6b8a4cb555c6f0973852dd5f949d685a6ef781d
danthegoodman1 <xxdanthegoodmanxx@gmail.com>
2018-04-25 16:09:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const HOST = '0.0.0.0';
 startExpress = (host, port) => {
     app = express();
     app.get('/', (req, res) => {
-        console.log("Request!");
-        res.status(200).send("Hello from nginx loadbalancer!!%%!");
+        console.log(`Request from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}`); // Either NGINX forward or just normal ip
+        res.status(200).send(`Hello from nginx loadbalancer on port ${port}`);
     });
     app.listen(port, host, function(){
         console.log(`Started listneing at ${host} on port ${port}`);
